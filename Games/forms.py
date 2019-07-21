bd6e1ea2c89c26b783973f4da11e2e1242156725
Aman Riat <amanriat777@gmail.com>
2019-07-20 23:52:36 -0400

    Finished main styling

--- Games/forms.py
+++ Games/forms.py
@@ -7,12 +7,5 @@ class DateForm(forms.Form):
     start_date = forms.DateField(widget=DateInput,label='Start Date', input_formats= ['%Y-%m-%d'])
     end_date = forms.DateField(widget=DateInput,label='End Date', input_formats=['%Y-%m-%d'])
 
-    # #Override
-    # def clean_start_date(self):
-    #     data = self.cleaned_data['start_date']
-    #
-    #     # if data > data + datetime.timedelta(days=365):
-    #     #     raise ValidationError(_('Invalid date - too old'))
-    #     return data
 
 
