16d1314da230a4acce0ad3a71cc5865ee83dc369
Aman Riat <amanriat777@gmail.com>
2019-09-15 19:40:31 -0400

    Updated reference to spreadsheet

--- Games/app_code/main_schedule.py
+++ Games/app_code/main_schedule.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import datetime
 
-file = os.path.realpath(r'./static/Games/NBA_18_19.xls')
+file = os.path.realpath(r'./static/Games/NBA-2019-20.xls')
 data = pd.read_excel(file)
 
 def convert_time(date):
